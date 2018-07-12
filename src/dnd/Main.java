2580ef1e15d151b774a5ec6c041c351c2c72fca4
WongZ <WongZ@192.168.200.235>
2018-07-11 23:44:25 -0400

    kill me wolfgang please help us

--- src/dnd/Main.java
+++ src/dnd/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
   
     public static void main(String[] args) {
-      Character zach = new Character("Elres", Race.randomRace(), 'm',"ranger", 1);
-      Character joe= new Character();
-      System.out.println(zach);
+      
+      Character assface= new Character();
+      
+      System.out.println(Race.randomNameGenerator(assface));
+     
       System.out.println("OKAY GENERATING A NEW COMPLETE RANDOMIZED CHARACTER");
-      System.out.println(joe);
+      
       
       
       
